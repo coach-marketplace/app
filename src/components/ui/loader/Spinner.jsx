6c6268109b073a1fa1047b346d168d6a4a2f3a0f
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-14 12:13:57 +0100

    Create Spinner UI component

---
+++ src/components/ui/loader/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Spinner as SpinnerUI } from "evergreen-ui";
+
+const Spinner = props => <SpinnerUI {...props} />;
+
+Spinner.displayName = "Spinner";
+
+export default Spinner;
