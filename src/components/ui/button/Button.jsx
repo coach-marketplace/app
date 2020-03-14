f405714cebd31e2f00adadcd9c523439e92f6d3b
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-14 12:06:23 +0100

    Simplify the Button

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,61 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Button as ButtonUI } from "evergreen-ui";
 
-import { SIZE, SIZES } from "../../../helper/constants";
-
-const Button = ({
-  children,
-  onClick,
-  label,
-  appearance,
-  intent,
-  size,
-  isLoading,
-  iconBefore,
-  iconAfter,
-  isDisabled,
-  type
-}) => (
-  <ButtonUI
-    appearance={appearance}
-    height={SIZE[size.toUpperCase()]}
-    onClick={onClick}
-    intent={intent}
-    isLoading={isLoading}
-    iconBefore={iconBefore}
-    iconAfter={iconAfter}
-    disabled={isDisabled}
-    type={type}
-  >
-    {children || label}
-  </ButtonUI>
+const Button = ({ children, label, ...props }) => (
+  <ButtonUI {...props}>{children || label}</ButtonUI>
 );
-
-Button.propTypes = {
-  appearance: PropTypes.oneOf(["default", "primary", "minimal"]),
-  children: PropTypes.node,
-  iconAfter: PropTypes.node,
-  iconBefore: PropTypes.node,
-  intent: PropTypes.oneOf(["none", "success", "warning", "danger"]),
-  isLoading: PropTypes.bool,
-  isDisabled: PropTypes.bool,
-  disabled: PropTypes.bool,
-  label: PropTypes.string,
-  onClick: PropTypes.func,
-  size: PropTypes.oneOf(SIZES)
-};
-
-Button.defaultProps = {
-  appearance: "default",
-  iconAfter: undefined,
-  iconBefore: undefined,
-  intent: "none",
-  isLoading: false,
-  disabled: false,
-  size: "m",
-  type: "text",
-  isDisabled: false
-};
 
 export default Button;
