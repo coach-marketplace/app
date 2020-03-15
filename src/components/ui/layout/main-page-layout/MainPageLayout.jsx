d85b80111e3266e187f3f34d3a60750c29cc2d54
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-15 08:25:44 +0100

    Refactor: replace custom header by evergreen Pane

--- src/components/ui/layout/main-page-layout/MainPageLayout.jsx
+++ src/components/ui/layout/main-page-layout/MainPageLayout.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import { Pane } from "evergreen-ui";
 
-import { Container, Header, Main, Footer } from "./styled";
+import { Container, Main, Footer } from "./styled";
 
 class MainPageLayout extends Component {
   static displayName = "MainPageLayout";
@@ -10,7 +11,16 @@ class MainPageLayout extends Component {
 
     return (
       <Container>
-        <Header>{header}</Header>
+        <Pane
+          is="header"
+          display="flex"
+          padding={16}
+          background="tint2"
+          borderRadius={3}
+          alignItems="center"
+        >
+          {header}
+        </Pane>
         <Main>{main}</Main>
         {footer && <Footer>{footer}</Footer>}
       </Container>
--- src/components/ui/layout/main-page-layout/styled.js
+++ src/components/ui/layout/main-page-layout/styled.js
@@ -6,22 +6,14 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
-export const Header = styled.header`
-  display: flex;
-  width: 100%;
-  height: 70px;
-  min-height: 70px;
-  background-color: AliceBlue;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
-`;
+
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   padding: 50px 10%;
 `;
+
 export const Footer = styled.footer`
   display: flex;
   flex-direction: column;
