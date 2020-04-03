da08a35c6170c637abad200fb3d292f3f067579c
Kevin Tassi <kevin.tassi@gmail.com>
2020-04-03 22:03:23 +0200

    Improve spinner component

--- src/components/ui/loader/Spinner.jsx
+++ src/components/ui/loader/Spinner.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 import { Spinner as SpinnerUI } from "evergreen-ui";
 
-const Spinner = props => <SpinnerUI {...props} />;
+const StyledContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Spinner = (props) => (
+  <StyledContainer>
+    <SpinnerUI {...props} />
+  </StyledContainer>
+);
 
 Spinner.displayName = "Spinner";
 
