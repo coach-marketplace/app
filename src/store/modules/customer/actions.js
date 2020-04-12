d7ff5ee5c6417881a1ec844e62aa699e36aa7a4e
Kevin Tassi <kevin.tassi@gmail.com>
2020-04-12 13:50:28 +0200

    imp: handle customer in store

--- src/store/modules/customer/actions.js
+++ src/store/modules/customer/actions.js
@@ -1,88 +1,64 @@
 import API from "../../../services/api";
 
 import {
-  GET_ALL_FAILED,
-  GET_ALL_LOADING,
-  GET_ALL_SUCCESS,
+  GET_CUSTOMERS_FAILED,
+  GET_CUSTOMERS_LOADING,
+  GET_CUSTOMERS_SUCCESS,
   CREATE_LOADING,
   CREATE_FAILED,
-  CREATE_SUCCESS
+  CREATE_SUCCESS,
 } from "./constants";
 import store from "../../index";
 
-const getAllLoading = () => {
-  return { type: GET_ALL_LOADING };
-};
+const getAllLoading = () => ({ type: GET_CUSTOMERS_LOADING });
+const getAllSuccess = (payload) => ({ type: GET_CUSTOMERS_SUCCESS, payload });
+const getAllFailed = (error) => ({ type: GET_CUSTOMERS_FAILED, error });
 
-const getAllSuccess = payload => {
-  return { type: GET_ALL_SUCCESS, user: payload };
-};
+const createLoading = () => ({ type: CREATE_LOADING });
+const createSuccess = (payload) => ({ type: CREATE_SUCCESS, payload });
+const createFailed = (error) => ({ type: CREATE_FAILED, error });
 
-const getAllFailed = error => {
-  return { type: GET_ALL_FAILED, error };
-};
-
-const createLoading = () => {
-  return { type: CREATE_LOADING };
-};
-
-const createSuccess = payload => {
-  return { type: CREATE_SUCCESS, user: payload };
-};
-
-const createFailed = error => {
-  return { type: CREATE_FAILED, error };
-};
-
-export const getAll = () => {
-  return dispatch => {
+export const retrieveAll = () => {
+  return (dispatch) => {
     dispatch(getAllLoading());
     const {
-      auth: { authUser, token }
+      auth: { authUser },
     } = store.getState();
 
-    if (!authUser || !token) {
-      return;
-    }
-
     API.get(`coach/${authUser._id}/customers`)
-      .then(response => {
+      .then((response) => {
         dispatch(getAllSuccess(response.data));
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(getAllFailed(error.message));
       });
   };
 };
 
-export const create = data => {
-  return dispatch => {
+export const create = (data) => {
+  return (dispatch) => {
     dispatch(createLoading());
     const {
-      auth: { authUser, token }
+      auth: { authUser },
     } = store.getState();
-
-    if (!authUser || !token) {
-      return;
-    }
 
     const normalizedData = {
       email: data.email,
-      first_name: data.firstName,
-      last_name: data.lastName,
-      phone: data.phone
+      firstName: data.firstName,
+      lastName: data.lastName,
+      phone: data.phone,
     };
 
     API.post(`coach/${authUser._id}/customers`, normalizedData)
-      .then(response => {
-        const customerId = response.data.lead;
+      // .then(response => {
+      //   const customerId = response.data.lead;
 
-        return API.get(`coach/${authUser._id}/customers/${customerId}`);
-      })
-      .then(response => {
+      //   return API.get(`coach/${authUser._id}/customers/${customerId}`);
+      // })
+      .then((response) => {
         dispatch(createSuccess(response.data));
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(createFailed(error.message));
       });
   };
