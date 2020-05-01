74068fc40a1b8d6cb8deb9aede3faf444e6cf023
Kevin Tassi <kevin.tassi@gmail.com>
2020-05-01 23:06:13 +0200

    imp: handle the login with google from login page

--- src/pages/auth-page/LoginPage.jsx
+++ src/pages/auth-page/LoginPage.jsx
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
+import queryString from "query-string";
 
 import { Title, toaster, Button } from "../../components/ui";
 import Layout from "../../components/layout/main-page-layout/MainPageLayout";
 import LoginForm from "../../components/auth/login-form/LoginForm";
 import * as actions from "../../store/modules/auth/actions";
+
+/**
+ * Get Filtered Query String
+ *
+ * When we redirect to the backend endpoint to connect with OAuth, we send
+ * the url containing the query string, but if we allow everything, the user
+ * can introduce data. To control that, we filter query string and only send
+ * the requestUrl.
+ * It also prevent to send the token and have some conflict
+ *
+ * @param {string} search Search part of the url
+ * @return {string} The allowed queryString for the login with OAuth
+ */
+const getFilteredQueryString = (search) => {
+  const parsed = queryString.parse(search);
+  const filtered = {};
+  parsed.requestUri && (filtered.requestUri = parsed.requestUri);
+
+  const qs = queryString.stringify(filtered);
+
+  return qs ? `?${qs}` : "";
+};
 
 const LoginPage = ({
   isLoginError,
@@ -14,6 +37,7 @@ const LoginPage = ({
   user,
   history,
   login,
+  location,
 }) => {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -31,8 +55,16 @@ const LoginPage = ({
   };
 
   const onLoginWithGoogle = () => {
-    console.log("google");
-    window.location = `${process.env.REACT_APP_API_URL}v1/auth/login-google`;
+    const { pathname, search } = location;
+    /**
+     * Don't forget to remove the last `/` from the base url
+     */
+    const completeCurrentUrl = `${process.env.REACT_APP_BASE_URL.slice(
+      0,
+      -1
+    )}${pathname}${getFilteredQueryString(search)}`;
+
+    window.location = `${process.env.REACT_APP_API_URL}v1/auth/login-google?isCoach=true&redirectUrl=${completeCurrentUrl}`;
   };
 
   const goToRegisterPage = () => history.push("/register");
